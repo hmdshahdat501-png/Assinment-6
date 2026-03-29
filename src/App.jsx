91d01b64e5bdf636f81f3815352db71caa10ae42
hmdshahdat501-png <hmdshahdat501@gmail.comm>
2026-03-29 23:00:03 +0600

    navber commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import './App.css'
-import Nav from './assets/Component/Nav/Nav'
+import Nav from './assets/Comontnt/Nav/Nav'
 
 function App() {
 
   return (
     <>
-   <div className='mx-auto container'>
+  <div className='mx-auto container'>
      <Nav></Nav>
-   </div>
+  </div>
     </>
   )
 }
---
+++ src/assets/Comontnt/Nav/Nav.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { FaShoppingCart } from "react-icons/fa";
+
+
+const Nav = () => {
+    return (
+        <div className='flex justify-between mt-6 items-center'> 
+            <h2 className='text-5xl font-bold text-blue-700'>DigiTolls</h2>
+            <ul className='flex gap-4'>
+                <li>Products</li>
+                <li>Features</li>
+                <li>Pricing</li>
+                <li>Testimonials</li>
+                <li>FAQ</li>
+            </ul>
+            <div className='flex items-center gap-4'>
+                <div class="indicator">
+  <span class="indicator-item badge badge-secondary">12</span>
+  <button class="btn"><FaShoppingCart />
+</button>
+</div>
+<div className='flex items-center gap-4'>
+      <p>Login</p>
+            <button className='btn  bg-linear-to-r from-purple-500 to-blue-500 text-white'>Get Started</button>
+</div>
+            </div>
+          
+        </div>
+    );
+};
+
+export default Nav;
